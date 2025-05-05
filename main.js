08ca4d2b7f7eabd00887cea19e60e32563e050a2
Abdelrahman Sayed <110208261+Abdelrahman0963@users.noreply.github.com>
2025-05-05 17:05:19 +0300

    Video Downloader first commi

---
+++ main.js
@@ -0,0 +1,38 @@
+let btn = document.querySelector(".btn-language");
+let languageList = document.querySelector(".language-list");
+btn.addEventListener("click", (outPord) => {
+    outPord.stopPropagation();
+    languageList.classList.toggle("btn-show");
+});
+
+document.addEventListener("click", (outPord) => {
+    if (!languageList.contains(outPord.target) && !btn.contains(outPord.target)) {
+        languageList.classList.remove("btn-show");
+    }
+});
+let past = document.getElementById("paste");
+
+past.addEventListener("click", () => {
+    const her = document.querySelector('.hero-spinner');
+    her.innerHTML = `
+      <div class="spinner">
+          <div class="bounce1"></div>
+          <div class="bounce2"></div>
+          <div class="bounce3"></div>
+      </div>
+    `;
+    navigator.clipboard.readText().then((text) => {
+        document.getElementById("input").value = text;
+    });
+
+    setTimeout(() => {
+        her.innerHTML = "";
+    }, 1000);
+});
+
+let pastTow = document.getElementById("paste");
+if (innerWidth < 768) {
+    pastTow.innerHTML = `<i class="fa-solid fa-paste"></i>`;
+} else {
+    pastTow.innerHTML = `<i class="fa-solid fa-paste"></i> PASTE`;
+}
---
+++ main2.js
@@ -0,0 +1,47 @@
+const translations = {
+    ar: {
+        languageText: 'اللغه <i class="fa-solid fa-angle-down"></i>',
+        fontFamily: '"Noto Kufi Arabic", sans-serif',
+        title: 'تنزيل <span style="color:#db168f">فيديو انستغرام</span> بسرعة وسهولة مع SaveMedia',
+        description: 'SaveMedia هو تطبيق يسمح لك باستخدام الوسائل التي تستخدمها لتنزيل فيديوهاتك من بسرعة وسهولة'
+    },
+    en: {
+        languageText: 'Language <i class="fa-solid fa-angle-down"></i>',
+        fontFamily: '"Cal Sans", sans-serif',
+        title: 'Fast and Easy <span style="color:#db168f">Instagram videos</span> Download With SaveMedia',
+        description: 'SaveMedia is an app that allows you to use the tools you use to download your videos quickly and easily.'
+    },
+    fr: {
+        languageText: 'la langue <i class="fa-solid fa-angle-down"></i>',
+        fontFamily: '"Cal Sans", sans-serif',
+        title: 'Téléchargement rapide et facile de <span style="color:#db168f">Videos Instagram</span> avec SaveMedia',
+        description: "SaveMedia est une application qui vous permet d'utiliser les moyens que vous utilisez pour télécharger vos vidéos à partir de vidéos Instagram rapidement et facilement."
+    }
+};
+document.getElementById("ar").addEventListener("click", () => changeLanguage("ar"));
+document.getElementById("en").addEventListener("click", () => changeLanguage("en"));
+document.getElementById("fr").addEventListener("click", () => changeLanguage("fr"));
+
+function changeLanguage(lang) {
+    const data = translations[lang];
+    const lun = document.getElementById("Language");
+    const lun2 = document.getElementById("title-site-h1");
+    const lun3 = document.getElementById("title-site-p");
+
+    if (window.innerWidth <= 768) {
+        lun.innerHTML = `<i class="fa-solid fa-globe"></i> <i class="fa-solid fa-angle-down"></i>`;
+    } else {
+        lun.innerHTML = data.languageText;
+        lun.style.fontFamily = data.fontFamily;
+        lun2.innerHTML = data.title;
+        lun2.style.fontFamily = data.fontFamily;
+        lun3.innerHTML = data.description;
+        lun3.style.fontFamily = data.fontFamily;
+    }
+
+    localStorage.setItem("lang", lang);
+}
+window.addEventListener("load", () => {
+    const savedLang = localStorage.getItem("lang") || "en";
+    changeLanguage(savedLang);
+});

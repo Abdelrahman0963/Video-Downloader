a09828a7f79d2422cc03e81d6d0389ab001840e2
Abdelrahman Sayed <110208261+Abdelrahman0963@users.noreply.github.com>
2025-05-05 21:21:39 +0300

    update in main2

--- main2.js
+++ main2.js
@@ -3,25 +3,29 @@ const translations = {
         languageText: 'اللغه <i class="fa-solid fa-angle-down"></i>',
         fontFamily: '"Noto Kufi Arabic", sans-serif',
         title: 'تنزيل <span style="color:#db168f">فيديو انستغرام</span> بسرعة وسهولة مع SaveMedia',
-        description: 'SaveMedia هو تطبيق يسمح لك باستخدام الوسائل التي تستخدمها لتنزيل فيديوهاتك من بسرعة وسهولة'
+        description: 'SaveMedia هو تطبيق يسمح لك باستخدام الوسائل التي تستخدمها لتنزيل فيديوهاتك من بسرعة وسهولة',
+        buttonText: 'تنزيل'
     },
     en: {
         languageText: 'Language <i class="fa-solid fa-angle-down"></i>',
         fontFamily: '"Cal Sans", sans-serif',
         title: 'Fast and Easy <span style="color:#db168f">Instagram videos</span> Download With SaveMedia',
-        description: 'SaveMedia is an app that allows you to use the tools you use to download your videos quickly and easily.'
+        description: 'SaveMedia is an app that allows you to use the tools you use to download your videos quickly and easily.',
+        buttonText: 'Download'
     },
     fr: {
         languageText: 'la langue <i class="fa-solid fa-angle-down"></i>',
         fontFamily: '"Cal Sans", sans-serif',
         title: 'Téléchargement rapide et facile de <span style="color:#db168f">Videos Instagram</span> avec SaveMedia',
-        description: "SaveMedia est une application qui vous permet d'utiliser les moyens que vous utilisez pour télécharger vos vidéos à partir de vidéos Instagram rapidement et facilement."
+        description: "SaveMedia est une application qui vous permet d'utiliser les moyens que vous utilisez pour télécharger vos vidéos à partir de vidéos Instagram rapidement et facilement.",
+        buttonText: 'Télécharger'
     },
     it: {
         languageText: 'Lingua<i class="fa-solid fa-angle-down"></i>',
         fontFamily: '"Italiana", sans-serif',
         title: 'Scarica <span style="color:#db168f">video Instagram</span> in modo facile e veloce con SaveMedia',
         description: 'Save Media è un app che ti consente di utilizzare gli strumenti che usi per scaricare i tuoi video in modo rapido e semplice.',
+        buttonText: 'Scarica'
     }
 };
 document.getElementById("ar").addEventListener("click", () => changeLanguage("ar"));
@@ -35,6 +39,7 @@ function changeLanguage(lang) {
     const lun = document.getElementById("Language");
     const lun2 = document.getElementById("title-site-h1");
     const lun3 = document.getElementById("title-site-p");
+    const btn = document.getElementById("donwload");
 
     lun.innerHTML = data.languageText;
     lun.style.fontFamily = data.fontFamily;
@@ -42,6 +47,7 @@ function changeLanguage(lang) {
     lun2.style.fontFamily = data.fontFamily;
     lun3.innerHTML = data.description;
     lun3.style.fontFamily = data.fontFamily;
+    btn.innerHTML = data.buttonText;
     if (window.innerWidth <= 768) {
         lun.innerHTML = `<i class="fa-solid fa-globe"></i> <i class="fa-solid fa-angle-down"></i>`;;
     } else {
